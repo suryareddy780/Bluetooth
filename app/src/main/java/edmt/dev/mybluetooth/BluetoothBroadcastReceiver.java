96ea90732300d51d5ef0d9b85bcc240c38505316
Hi <suryareddy780@gmail.com>
2021-02-08 19:31:49 +0530

    Bluetooth

---
+++ app/src/main/java/edmt/dev/mybluetooth/BluetoothBroadcastReceiver.java
@@ -0,0 +1,36 @@
+package edmt.dev.mybluetooth;
+
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+public class BluetoothBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+        Log.d("BroadcastActions", "Action "+action+"received");
+        int state;
+        BluetoothDevice bluetoothDevice;
+
+        switch (action)
+        {
+
+            case BluetoothDevice.ACTION_ACL_CONNECTED:
+                bluetoothDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                Toast.makeText(context, "Connected to "+bluetoothDevice.getName(),
+                        Toast.LENGTH_SHORT).show();
+                Log.d("BroadcastActions", "Connected to "+bluetoothDevice.getName());
+                break;
+
+            case BluetoothDevice.ACTION_ACL_DISCONNECTED:
+                bluetoothDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                Toast.makeText(context, "Disconnected from "+bluetoothDevice.getName(),
+                        Toast.LENGTH_SHORT).show();
+                Log.d("BroadcastActions","Disconnected from "+bluetoothDevice.getName());
+                break;
+        }
+    }
+}
